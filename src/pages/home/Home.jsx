b78039a4ebaa90f45e74c70722553f34ff45962f
Hasan-E <hasan.erhan1302@gmail.com>
2025-05-31 17:07:51 +0300

    update home and home css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-
+import "./Home.scss"
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <h4 className='homeText'>Hi! My Name Is</h4>
+      <h1 className='name'>Wednesday <span className='lastName'>Addams</span></h1>
+      <h4 className='homeText'>Full Stack Web Developer</h4>
+    </div>
   )
 }
 
