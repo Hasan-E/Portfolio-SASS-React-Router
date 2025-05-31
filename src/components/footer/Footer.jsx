243db734c31c45b1b7a55e765a5f414bb83f78fd
Hasan-E <hasan.erhan1302@gmail.com>
2025-05-31 03:14:07 +0300

    update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import { FaGithubSquare } from "react-icons/fa";
+import {
+  FaSquareFacebook,
+  FaSquareInstagram,
+  FaSquareXTwitter,
+} from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer >
+      <div className="icons">
+        <a
+          target="blank"
+          href="https://x.com/i/flow/login"
+          className="socialIcon"
+        >
+          <i>
+            <FaSquareXTwitter />
+          </i>
+        </a>
+        <a
+          target="blank"
+          href="https://www.facebook.com/?locale=tr_TR"
+          className="socialIcon"
+        >
+          <i>
+            <FaSquareFacebook />
+          </i>
+        </a>
+        <a
+          target="blank"
+          href="https://www.instagram.com/"
+          className="socialIcon"
+        >
+          <i>
+            <FaSquareInstagram />
+          </i>
+        </a>
+        <a
+          target="blank"
+          href="https://github.com/Hasan-E"
+          className="socialIcon"
+        >
+          <i>
+            <FaGithubSquare />
+          </i>
+        </a>
+      </div>
+      <div className="copyRight">
+        <p>© Copyright ROOSTER 2025</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
