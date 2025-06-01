b9c2fc08a60879cbee6bc826e7d41e29ed7a1858
Hasan-E <hasan.erhan1302@gmail.com>
2025-06-02 01:00:55 +0300

    update project and project css

--- src/helpers/imageData.jsx
+++ src/helpers/imageData.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
-const imageData = () => {
-  return (
-    <div>imageData</div>
-  )
-}
-
-export default imageData
+import img1 from "../assets/img/project-1.jpg";
+import img2 from "../assets/img/project-2.jpg";
+import img3 from "../assets/img/project-3.jpg";
+import img4 from "../assets/img/project-4.jpg";
+import img5 from "../assets/img/project-5.jpg";
+import img6 from "../assets/img/project-6.jpg";
+export const data = [
+  {
+    id: 1,
+    img: img1,
+  },
+  {
+    id: 2,
+    img: img2,
+  },
+  {
+    id: 3,
+    img: img3,
+  },
+  {
+    id: 4,
+    img: img4,
+  },
+  {
+    id: 5,
+    img: img5,
+  },
+  {
+    id: 6,
+    img: img6,
+  },
+];
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import { data } from "../../helpers/imageData";
+import "./Projects.scss"
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className="projectContainer">
+      <h2 className="projectTitle">My Projects</h2>
+      <div className="projects">
+        {data.map((project) => (
+          <div key={project.id} className="imgDiv">
+            <img src={project.img} alt={project.id} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
