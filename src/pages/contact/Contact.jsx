b75771bad14dac44b66d23514dddb6195f7123cc
Hasan-E <hasan.erhan1302@gmail.com>
2025-06-02 16:14:20 +0300

    update contact and contact css

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { HiOutlineLocationMarker, HiOutlineMail, HiOutlinePhone } from "react-icons/hi";
+import "./Contact.scss"
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="contactContainer">
+      <h1 className="contactTitle">Contact Me...</h1>
+      <div className="contactItems">
+        <div className="item">
+          <p className="itemName">
+            <i>
+              <HiOutlineMail />
+            </i>
+            Email
+          </p>
+          <p className="itemPath">wednesday@nevermoreacademy.edu</p>
+        </div>
+        <div className="item">
+          <p className="itemName">
+            <i>
+              <HiOutlinePhone />
+            </i>
+            Phone
+          </p>
+          <p className="itemPath">+40 (571) 360-1234</p>
+        </div>
+        <div className="item">
+          <p className="itemName">
+            <i>
+              <HiOutlineLocationMarker />
+            </i>
+            Address
+          </p>
+          <p className="itemPath">Strada Zamorei 1, Bușteni 105500, Romania</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
